4b6e1d278ce09a8baac75b272ff49acb02226330
Zubair N. Ahmed <zubair.ahmed@alumni.stanford.edu>
2020-02-01 16:06:12 -0800

    style the user input

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,20 +1,12 @@
-import React, { Component, useState, useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom';
-import Popover from '@material-ui/core/Popover'
-
-import moment from 'moment'
-
+import React from 'react'
+import { Button } from '@material-ui/core'
 import './User.scss'
 
 import ProfileIcon from './images/header-person-icon.png'
 import CELCornerLogo from './images/cel-corner-logo.png'
 import EnergyLogo from './images/Icon_HomeEnergyMonitor.png'
 
-
-
 function User({ history }) {
-  
-
   return (
     <div className="User">
       <>
@@ -41,7 +33,6 @@ function User({ history }) {
           src={EnergyLogo}
           alt="cel logo"
         />
-
         <form className="UserInputForm" onSubmit={() => history.push('/app')}>
           <div>
           <label>
@@ -50,10 +41,23 @@ function User({ history }) {
             <input type="text" name="name"/>
           </label>
           </div>
-          <input type="submit" value="Submit" className="UserInputBox"/>
+          {/* <input type="submit" value="Submit" className="UserInputBox"/> */}
+          <Button
+            type="submit"
+            color="#00B189"
+            variant="contained"
+            size="large"
+            style={{
+              width: '3rem', width: '18rem', height: '3rem',
+              letterSpacing: '3px',
+          backgroundColor: '#00B189',
+          color: 'white',
+          fontWeight: 'bold',
+          marginTop: '2rem', fontSize: '1.2rem' }}
+                >
+                Submit
+          </Button>
         </form>
-
-
       </div>
     </div>
   )
